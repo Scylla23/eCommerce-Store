7bfe5e0ccdbeeac8be9e95be1aa7670ea2d8a87f
Scylla23 <2019bcs044@sggs.ac.in>
2023-09-21 21:06:43 +0530

    featured products

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-const HomePage = () => {
-    return ( 
-        <div>Hello Store</div>
-     );
-}
- 
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("4ab74563-dbab-46e7-adc5-5508d4225182");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
 export default HomePage;
